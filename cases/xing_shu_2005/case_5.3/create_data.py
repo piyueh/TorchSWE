e969a1122fffdf0d27978e91bf5f2bf9aca390f1
Pi-Yueh Chuang <pychuang@gwu.edu>
2020-03-28 06:09:45 -0400

    add case 5.3 from Xing & Shu

---
+++ cases/xing_shu_2005/case_5.3/create_data.py
@@ -0,0 +1,82 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2020 Pi-Yueh Chuang <pychuang@gwu.edu>
+#
+# Distributed under terms of the MIT license.
+
+"""
+Create topography and I.C. file for case 5.3 in Xing and Shu (2005).
+
+Note, the elevation data in the resulting NetCDF file is defined at vertices,
+instead of cell centers. But the I.C. is defined at cell centers.
+"""
+import os
+import yaml
+import numpy
+
+def main():
+    """Main function"""
+
+    # it's users' responsibility to make sure TorchSWE package can be found
+    from TorchSWE.utils.netcdf import write_cf
+
+    me = os.path.abspath(__file__)
+    case = os.path.dirname(me)
+
+    with open(os.path.join(case, "config.yaml"), 'r') as f:
+        config = yaml.load(f, Loader=yaml.CLoader)
+
+    x = numpy.linspace(
+        config["domain"]["west"], config["domain"]["east"],
+        config["discretization"]["Nx"]+1, dtype=numpy.float64)
+    y = numpy.linspace(
+        config["domain"]["south"], config["domain"]["north"],
+        config["discretization"]["Ny"]+1, dtype=numpy.float64)
+
+    # 2D X, Y for temporarily use
+    X, Y = numpy.meshgrid(x, y)
+
+    # topogeaphy elevation
+    B = 0.8 * numpy.exp(-5.*numpy.power(X-0.9, 2)-50.*numpy.power(Y-0.5, 2))
+
+    # write topography file
+    write_cf(
+        os.path.join(case, config["topography"]["file"]), x, y,
+        {config["topography"]["key"]: B},
+        options={config["topography"]["key"]: {"units": "m"}})
+
+    # x and y for cell centers
+    xc = (x[:-1] + x[1:]) / 2.
+    yc = (y[:-1] + y[1:]) / 2.
+
+    # I.C.: w
+    w = numpy.ones_like(xc)
+    w[(xc>=0.05)*(xc<=0.15)] += 0.01
+    w = numpy.tile(w, (config["discretization"]["Ny"], 1))
+
+    # I.C.: hu & hv
+    hu = numpy.zeros_like(xc)
+    hv = numpy.zeros_like(xc)
+    hu = numpy.tile(hu, (config["discretization"]["Ny"], 1))
+    hv = numpy.tile(hv, (config["discretization"]["Ny"], 1))
+
+    # write I.C. file
+    data = dict(zip(config["ic"]["keys"], [w, hu, hv]))
+    write_cf(
+        os.path.join(case, config["ic"]["file"]), xc, yc,
+        dict(zip(config["ic"]["keys"], [w, hu, hv])),
+        options=dict(zip(config["ic"]["keys"], [
+            {"units": "m"}, {"units": "m2 s-1"}, {"units": "m2 s-1"}])))
+
+if __name__ == "__main__":
+    import sys
+
+    # when execute this script directly, make sure TorchSWE can be found
+    pkg_path = os.path.dirname(os.path.dirname(os.path.dirname(
+        os.path.dirname(os.path.dirname(os.path.abspath(__file__))))))
+    sys.path.append(pkg_path)
+
+    # execute the main function
+    main()
---
+++ cases/xing_shu_2005/case_5.3/plot.py
@@ -0,0 +1,81 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2020 Pi-Yueh Chuang <pychuang@gwu.edu>
+#
+# Distributed under terms of the MIT license.
+
+"""
+Plot.
+"""
+import os
+import numpy
+from matplotlib import pyplot
+
+def main():
+    """Plot and compare to analytical solutions."""
+
+    # it's users' responsibility to make sure TorchSWE package can be found
+    from TorchSWE.utils.netcdf import read_cf
+
+    # read simulation data
+    filename = os.path.join(os.path.dirname(os.path.abspath(__file__)), "solutions.nc")
+    sim_data, _ = read_cf(filename, ["w"])
+
+    # 2D coordinates
+    x = sim_data["x"]
+    y = sim_data["y"]
+    X, Y = numpy.meshgrid(x, y)
+
+    # get solutions except the one at T=0
+    W = sim_data["w"][1:, :, :]
+
+    # time labels
+    t = [0.12, 0.24, 0.36, 0.48, 0.6]
+
+    # contour line range
+    n = 32
+    r = [
+        numpy.linspace(0.999703, 1.00629, n),
+        numpy.linspace(0.994836, 1.01604, n),
+        numpy.linspace(0.988582, 1.0117, n),
+        numpy.linspace(0.990344, 1.00497, n),
+        numpy.linspace(0.995065, 1.0056, n)]
+
+    # contour lines: to compare with Xing & Shu
+    for i in range(5):
+        pyplot.figure(figsize=(10, 4), dpi=166)
+        pyplot.contour(X, Y, W[i], r[i], linewidths=1)
+        pyplot.title("Xing & Shu (2005) case 5.3: water level @ T={} sec".format(t[i]))
+        pyplot.xlabel("x (m)")
+        pyplot.ylabel("y (m)")
+        pyplot.xlim(0., 2.)
+        pyplot.ylim(0., 1.)
+        pyplot.colorbar()
+        pyplot.tight_layout()
+        pyplot.savefig("water_level_contourline_t={}.png".format(t[i]), dpi=166, bbox_inches="tight")
+
+    # contourf
+    for i in range(5):
+        pyplot.figure(figsize=(10, 4), dpi=166)
+        pyplot.contourf(X, Y, W[i], 128)
+        pyplot.title("Xing & Shu (2005) case 5.3: water level @ T={} sec".format(t[i]))
+        pyplot.xlabel("x (m)")
+        pyplot.ylabel("y (m)")
+        pyplot.xlim(0., 2.)
+        pyplot.ylim(0., 1.)
+        pyplot.colorbar()
+        pyplot.tight_layout()
+        pyplot.savefig("water_level_contour_t={}.png".format(t[i]), dpi=166, bbox_inches="tight")
+
+if __name__ == "__main__":
+    import sys
+
+    # when execute this script directly, make sure TorchSWE can be found
+    pkg_path = os.path.dirname(os.path.dirname(os.path.dirname(
+        os.path.dirname(os.path.dirname(os.path.abspath(__file__))))))
+    sys.path.append(pkg_path)
+
+    # execute the main function
+    main()
