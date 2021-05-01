993d8ef29c5b17afa1394628c48a313280ed3cbd
Pi-Yueh Chuang <pychuang@gwu.edu>
2021-05-01 15:23:27 -0400

    use numpy for limiters.py

--- torchswe/core/limiters.py
+++ torchswe/core/limiters.py
@@ -8,45 +8,55 @@
 
 """Slope/flux limiters.
 """
-import torch
+import numpy
 
-@torch.jit.script
-def minmod_limiter(U, dx: float, Ngh: int, theta: float=1.3):
-    """Calculating slope based on minmod.
 
-    Args:
-    -----
-        U: a 3D (3, Ny+2*Ngh, Nx+2*Ngh) torch.tensor of conservative variables
-            at cell centers.
-        dx: a scalar of cell szie, assuming dx = dy and is uniform everywhere.
-        theta: parameter to controll oscillation and dispassion.
+def minmod_slope(quantity, delta_x, delta_y, n_ghost, theta, tol=1e-12):
+    """Calculate minmod slopes in x- and y-direction of quantity.
+
+    Arguments
+    ---------
+    quantity : (Ny+2*n_ghost, Nx+2*n_ghost) numpy.ndarray
+        Target quantity. For example, w, hu, or hv.
+    delta_x, delta_y : float
+        Cell size in x- and y-direction. Currently only supports constant cell sizes.
+    n_ghost : int
+        Number of ghost cell layers outsied each boundary.
+    theta: float
+        Parameter to controll oscillation and dispassion. 1 <= theta <= 2.
+    tol : float
+        To control how small can be treat as zero.
 
     Return:
     -------
-        dUx: a (3, Ny, Nx+2) torch.tensor for $\partial U / \partial x$ at cell
-            centers. Only one layer of ghost cells is included at each domain
-            boundary in x direction.
-        dUy: a (3, Ny+2, Nx) torch.tensor for $\partial U / \partial y$ at cell
-            centers. Only one layer of ghost cells is included at each domain
-            boundary in y direction.
+    slope_x : (Ny, Nx+2) numpy.ndarray
+        Slopes in x-direction, including one layer of ghost cells in x-direction.
+    slope_y : (Ny+2, Nx) numpy.ndarray
+        Slopes in y-direction, including one layer of ghost cells in y-direction.
     """
 
-    # for convenience
-    Ny = U.shape[1] - 2 * Ngh
-    Nx = U.shape[2] - 2 * Ngh
-    zero = torch.tensor(0., dtype=U.dtype, device=U.device)
-    theta = torch.tensor(theta, dtype=U.dtype, device=U.device)
+    # x-direction
+    denominator = quantity[:, 2:]-quantity[:, 1:-1]  # q_{i+1} - q_{i}
+    slope_x = numpy.divide(
+        quantity[:, 1:-1]-quantity[:, :-2], denominator, out=numpy.zeros_like(denominator),
+        where=numpy.logical_not(numpy.isclose(denominator, 0., atol=tol))
+    )  # inf and NaN from division will just be zeros
+    slope_x = numpy.maximum(numpy.minimum(theta*slope_x, (1.+slope_x)/2., theta), 0.)
+    slope_x *= denominator
+    slope_x /= delta_x
+    slope_x[numpy.isclose(slope_x, 0., atol=tol)] = 0.  # hard-code to zero for slope < tol
 
-    numerator = U[:, Ngh:-Ngh, Ngh-1:-Ngh+1]-U[:, Ngh:-Ngh, Ngh-2:-Ngh] # U_{i} - U_{i-1}
-    denominator = U[:, Ngh:-Ngh, Ngh:Nx+Ngh+2]-U[:, Ngh:-Ngh, Ngh-1:-Ngh+1] # U_{i+1} - U_{i}
-    r = numerator / denominator
-    phi = torch.max(torch.min(torch.min(theta*r, (1.+r)/2.), theta), zero)
-    dUx = torch.where(torch.isnan(r), zero, phi*denominator/dx)
+    # y-direction
+    denominator = quantity[2:, :]-quantity[1:-1, :]  # q_{i+1} - q_{i}
+    slope_y = numpy.divide(
+        quantity[1:-1, :]-quantity[:-2, :], denominator, out=numpy.zeros_like(denominator),
+        where=numpy.logical_not(numpy.isclose(denominator, 0., atol=tol))
+    )  # inf and NaN from division will just be zeros
+    slope_y = numpy.maximum(numpy.minimum(theta*slope_y, (1.+slope_y)/2., theta), 0.)
+    slope_y *= denominator
+    slope_y /= delta_y
+    slope_y[numpy.isclose(slope_y, 0., atol=tol)] = 0.  # hard-code to zero for slope < tol
 
-    numerator = U[:, Ngh-1:-Ngh+1, Ngh:-Ngh]-U[:, Ngh-2:-Ngh, Ngh:-Ngh] # U_{j} - U_{j-1}
-    denominator = U[:, Ngh:Ny+Ngh+2, Ngh:-Ngh]-U[:, Ngh-1:-Ngh+1, Ngh:-Ngh] # U_{j+1} - U_{j}
-    r = numerator / denominator
-    phi = torch.max(torch.min(torch.min(theta*r, (1.+r)/2.), theta), zero)
-    dUy = torch.where(torch.isnan(r), zero, phi*denominator/dx)
-
-    return dUx, dUy
+    return \
+        slope_x[n_ghost:-n_ghost, n_ghost-1:-n_ghost+1], \
+        slope_y[n_ghost-1:-n_ghost+1, n_ghost:-n_ghost]
