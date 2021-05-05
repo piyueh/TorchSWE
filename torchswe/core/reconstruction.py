6aff13be6ced56c8214761d1e72dcbf29f44398a
Pi-Yueh Chuang <pychuang@gwu.edu>
2021-05-05 15:32:33 -0400

    workaround for legate in fixing negative depth

--- torchswe/core/reconstruction.py
+++ torchswe/core/reconstruction.py
@@ -12,6 +12,10 @@ from torchswe import nplike
 from torchswe.utils.data import States, Gridlines, Topography
 
 
+# TODO: in legate's version, check if using for-loops is slower or faster than using `where`. The
+#       rationale behind using a for-loop is that the number of negative depths may be very small
+
+
 def correct_negative_depth(states: States, topo: Topography) -> States:
     """Fix negative depth on the both sides of cell faces.
 
@@ -25,6 +29,31 @@ def correct_negative_depth(states: States, topo: Topography) -> States:
     states : torchswe.utils.data.States
         The same object as the input. Changed inplace. Returning it just for coding style.
     """
+
+    if nplike.__name__ == "legate.numpy":
+        return correct_negative_depth_legate(states, topo)
+    else:
+        return correct_negative_depth_regular(states, topo)
+
+
+
+def correct_negative_depth_regular(states: States, topo: Topography) -> States:
+    """Fix negative depth on the both sides of cell faces.
+
+    Arguments
+    ---------
+    states : torchswe.utils.data.States
+    topo : torchswe.utils.data.Topography
+
+    Returns:
+    --------
+    states : torchswe.utils.data.States
+        The same object as the input. Changed inplace. Returning it just for coding style.
+    """
+
+    # legate.numpy's support of advanced indexing is buggy, so it has its own version of this func
+    if nplike.__name__ == "legate.numpy":
+        return correct_negative_depth_legate(states, topo)
 
     # fix the case when the left depth of an interface is negative
     j, i = nplike.nonzero(states.face.x.minus.w < topo.xface)
@@ -62,6 +91,65 @@ def correct_negative_depth(states: States, topo: Topography) -> States:
 
     j, i = nplike.nonzero(states.face.y.minus.w < topo.yface)
     states.face.y.minus.w[j, i] = topo.yface[j, i]
+
+    return states
+
+
+def correct_negative_depth_legate(states: States, topo: Topography) -> States:
+    """Legate's version of fixing negative depth on the both sides of cell faces.
+
+    Arguments
+    ---------
+    states : torchswe.utils.data.States
+    topo : torchswe.utils.data.Topography
+
+    Returns:
+    --------
+    states : torchswe.utils.data.States
+        The same object as the input. Changed inplace. Returning it just for coding style.
+    """
+
+    # alias; shorten the code
+    ngh = states.ngh
+
+    # fix the case when the left depth of an interface is negative
+    neg_j, neg_i = nplike.nonzero(states.face.x.minus.w < topo.xface)
+
+    # a workaround for Legate; let's hope neg_j and neg_i are not long
+    for j, i in zip(neg_j, neg_i):
+        j, i =  int(j), int(i)  # legate.numpy does not like numpy.int64
+        states.face.x.minus.w[j, i] = topo.xface[j, i]
+        if i > 0:
+            states.face.x.plus.w[j, i-1] = 2 * states.q.w[j+ngh, i-1+ngh] - topo.xface[j, i]
+
+    # fix the case when the right depth of an interface is negative
+    neg_j, neg_i = nplike.nonzero(states.face.x.plus.w < topo.xface)
+
+    for j, i in zip(neg_j, neg_i):
+        j, i =  int(j), int(i)  # legate.numpy does not like numpy.int64
+        states.face.x.plus.w[j, i] = topo.xface[j, i]
+        if i < states.nx:
+            states.face.x.minus.w[j, i+1] = 2 * states.q.w[j+ngh, i+ngh] - topo.xface[j, i]
+
+    # fix the case when the bottom depth of an interface is negative
+    neg_j, neg_i = nplike.nonzero(states.face.y.minus.w < topo.yface)
+
+    for j, i in zip(neg_j, neg_i):
+        j, i =  int(j), int(i)  # legate.numpy does not like numpy.int64
+        states.face.y.minus.w[j, i] = topo.yface[j, i]
+        if j > 1:
+            states.face.y.plus.w[j-1, i] = 2 * states.q.w[j-1+ngh, i+ngh] - topo.yface[j, i]
+
+    # fix the case when the top depth of an interface is negative
+    neg_j, neg_i = nplike.nonzero(states.face.y.plus.w < topo.yface)
+
+    for j, i in zip(neg_j, neg_i):
+        j, i =  int(j), int(i)  # legate.numpy does not like numpy.int64
+        states.face.y.plus.w[j, i] = topo.yface[j, i]
+        if j < states.ny:
+            states.face.y.minus.w[j+1, i] = 2 * states.q.w[j+ngh, i+ngh] - topo.yface[j, i]
+
+    # ignoring rounding error for now
 
     return states
 
