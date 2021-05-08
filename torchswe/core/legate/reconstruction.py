b0899bf8e52f65405eafa5a1de2511a06eca37da
Pi-Yueh Chuang <pychuang@gwu.edu>
2021-05-07 22:35:14 -0400

    use where to replace explicit for-loop in leagate codepath

--- torchswe/core/legate/reconstruction.py
+++ torchswe/core/legate/reconstruction.py
@@ -8,8 +8,11 @@
 
 """Linear reconstruction. (Legate version)
 """
+import logging
 from torchswe import nplike
 from torchswe.utils.data import States, Gridlines, Topography
+
+logger = logging.getLogger("torchswe.core.legate.reconstruction")
 
 
 # TODO: in legate's version, check if using for-loops is slower or faster than using `where`. The
@@ -33,43 +36,29 @@ def correct_negative_depth(states: States, topo: Topography) -> States:
     # alias; shorten the code
     ngh = states.ngh
 
-    # fix the case when the left depth of an interface is negative
-    neg_j, neg_i = nplike.nonzero(states.face.x.minus.w < topo.xface)
+    # aliases
+    qwslc = states.q.w[ngh:-ngh, ngh:-ngh]  # hopefully this is just a view in legate
+    x, y = states.face.x, states.face.y
 
-    # a workaround for Legate; let's hope neg_j and neg_i are not long
-    for j, i in zip(neg_j, neg_i):
-        j, i =  int(j), int(i)  # legate.numpy does not like numpy.int64
-        states.face.x.minus.w[j, i] = topo.xface[j, i]
-        if i > 0:
-            states.face.x.plus.w[j, i-1] = 2 * states.q.w[j+ngh, i-1+ngh] - topo.xface[j, i]
+    # fix the case when the left depth of an interface is negative
+    neg = (states.face.x.minus.w < topo.xface)
+    x.minus.w = nplike.where(neg, topo.xface, x.minus.w)
+    x.plus.w[:, :-1] = nplike.where(neg[:, 1:], 2*qwslc-topo.xface[:, 1:], x.plus.w[:, :-1])
 
     # fix the case when the right depth of an interface is negative
-    neg_j, neg_i = nplike.nonzero(states.face.x.plus.w < topo.xface)
-
-    for j, i in zip(neg_j, neg_i):
-        j, i =  int(j), int(i)  # legate.numpy does not like numpy.int64
-        states.face.x.plus.w[j, i] = topo.xface[j, i]
-        if i < states.nx:
-            states.face.x.minus.w[j, i+1] = 2 * states.q.w[j+ngh, i+ngh] - topo.xface[j, i]
+    neg = (x.plus.w < topo.xface)
+    x.plus.w = nplike.where(neg, topo.xface, x.plus.w)
+    x.minus.w[:, 1:] = nplike.where(neg[:, :-1], 2*qwslc-topo.xface[:, :-1], x.minus.w[:, 1:])
 
     # fix the case when the bottom depth of an interface is negative
-    neg_j, neg_i = nplike.nonzero(states.face.y.minus.w < topo.yface)
-
-    for j, i in zip(neg_j, neg_i):
-        j, i =  int(j), int(i)  # legate.numpy does not like numpy.int64
-        states.face.y.minus.w[j, i] = topo.yface[j, i]
-        if j > 1:
-            states.face.y.plus.w[j-1, i] = 2 * states.q.w[j-1+ngh, i+ngh] - topo.yface[j, i]
+    neg = (y.minus.w < topo.yface)
+    y.minus.w = nplike.where(neg, topo.yface, y.minus.w)
+    y.plus.w[:-1, :] = nplike.where(neg[1:, :], 2*qwslc-topo.yface[1:, :], y.plus.w[:-1, :])
 
     # fix the case when the top depth of an interface is negative
-    neg_j, neg_i = nplike.nonzero(states.face.y.plus.w < topo.yface)
-
-    for j, i in zip(neg_j, neg_i):
-        j, i =  int(j), int(i)  # legate.numpy does not like numpy.int64
-        states.face.y.plus.w[j, i] = topo.yface[j, i]
-        if j < states.ny:
-            states.face.y.minus.w[j+1, i] = 2 * states.q.w[j+ngh, i+ngh] - topo.yface[j, i]
+    neg = (y.plus.w < topo.yface)
+    y.plus.w = nplike.where(neg, topo.yface, y.plus.w)
+    y.minus.w[1:, :] = nplike.where(neg[:-1, :], 2*qwslc-topo.yface[:-1, :], y.minus.w[1:, :])
 
     # ignoring rounding error for now
-
     return states
