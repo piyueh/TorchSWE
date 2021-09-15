6abf1783e9a7695ed78de3e1ace62f4c8a5d415d
Pi-Yueh Chuang <pychuang@gwu.edu>
2021-09-15 17:31:26 -0400

    bump the package version in the code and update description

--- torchswe/__init__.py
+++ torchswe/__init__.py
@@ -6,7 +6,7 @@
 #
 # Distributed under terms of the BSD 3-Clause license.
 
-"""A GPU shallow-water equation solver.
+"""A shallow-water equation solver for pipeline landspill.
 """
 import os
 import logging
@@ -41,5 +41,5 @@ else:
     import numpy as nplike
 
 
-__version__ = "0.1.dev2"
+__version__ = "0.2"
 logger = logging.getLogger("torchswe")
