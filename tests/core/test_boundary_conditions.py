094e2945e1081620c90a967aafbe9c01665ef49c
Pi-Yueh Chuang <pychuang@gwu.edu>
2021-09-30 05:58:54 -0400

    update emails

--- tests/core/test_boundary_conditions.py
+++ tests/core/test_boundary_conditions.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # vim:fenc=utf-8
 #
-# Copyright © 2021 Pi-Yueh Chuang <pychuang@pm.me>
+# Copyright © 2021 Pi-Yueh Chuang <pychuang@gwu.edu>
 #
 # Distributed under terms of the BSD 3-Clause license.
 
