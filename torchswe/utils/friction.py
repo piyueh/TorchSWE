e98385483bfdd4de87fe293095bb7f15d2b43417
Pi-Yueh Chuang <pychuang@gwu.edu>
2021-10-12 17:27:08 -0400

    friction.py: changed to h, hu, hv; nplike -> _nplike

--- torchswe/utils/friction.py
+++ torchswe/utils/friction.py
@@ -8,7 +8,7 @@
 
 """A collection of Darcy-Weisbach friction coefficient models.
 """
-from torchswe import nplike
+from torchswe import nplike as _nplike
 
 
 def approx_lambert_w(x):  # pylint: disable=invalid-name
@@ -23,12 +23,12 @@ def approx_lambert_w(x):  # pylint: disable=invalid-name
     -------
     float or ndarray of floats.
     """
-    lnx = nplike.log(x)
-    lnlnx = nplike.log(lnx)
+    lnx = _nplike.log(x)
+    lnlnx = _nplike.log(lnx)
     return lnx - lnlnx + lnlnx / lnx + (lnlnx * lnlnx - 2. * lnlnx) / (2. * lnx * lnx)
 
 
-def bellos_et_al_2018(velocity, depth, viscosity, roughness):
+def bellos_et_al_2018(h, hu, hv, viscosity, roughness):
     """Bellow et al., 2018
 
     Note that this implementation slightly differs from paper because we recalculated the
@@ -36,50 +36,45 @@ def bellos_et_al_2018(velocity, depth, viscosity, roughness):
 
     Arguments
     ---------
-    velocity : nplike.ndarray or float
-        Velocity (magnitude).
-    depth : nplike.ndarray or float
-        Flow depth.
-    viscosity : nplike.ndarray or float
+    h, hu, hv : _nplike.ndarray or float
+        Depth and the conservative quantities in x and y directions.
+    viscosity : _nplike.ndarray or float
         Flow kinematic viscosity.
-    roughness : nplike.ndarray or float
+    roughness : _nplike.ndarray or float
         Absolute roughness with dimension of length.
 
     Returns
     -------
-    coefficients : nplike.ndarray or float
+    coefficients : _nplike.ndarray or float
         Darcy-Weisbach friction coefficients.
     """
     # pylint: disable=invalid-name
 
     # reynolds number defined by depth
-    re_h = velocity * depth / viscosity
+    re_h = _nplike.sqrt(hu**2 + hv**2) / viscosity
 
     # probability
-    alpha = nplike.reciprocal(nplike.power(re_h/678., 8.4)+1.)
-    beta = nplike.reciprocal(nplike.power(re_h*roughness/(150.*depth), 1.8) + 1.)
+    alpha = _nplike.reciprocal(_nplike.power(re_h/678., 8.4)+1.)
+    beta = _nplike.reciprocal(_nplike.power(re_h*roughness/(150.*h), 1.8) + 1.)
 
     # coefficient of laminar regime
-    C1 = nplike.power(
-        24. / re_h,
-        alpha
-    )
+    C1 = _nplike.power(24./re_h, alpha)
 
     # coefficient of smmoth turbulence regime
-    C2 = nplike.power(
-        0.7396119392950918 * nplike.exp(2.*approx_lambert_w(1.3484253036698046*re_h)) / re_h**2,
+    C2 = _nplike.power(
+        0.7396119392950918 * _nplike.exp(2.*approx_lambert_w(1.3484253036698046*re_h)) / re_h**2,
         (1. - alpha) * beta
     )
 
-    # in case roughness == 0; beta is also 0 in this case, so it becomes inf^0 -> 1
     try:
-        with nplike.errstate(divide="ignore"):  # numpy won't complain
+        # if roughness == 0, (1. - beta) is also 0, so C3 becomes inf^0 -> 1
+        with _nplike.errstate(divide="ignore"):  # numpy/cupy won't complain division by 0
             # coefficient of fully-rough turbulence regime
-            C3 = nplike.power(
-                1.3447999999999998 / nplike.log(12.213597446891887 * depth / roughness)**2,
+            C3 = _nplike.power(
+                1.3447999999999998 / _nplike.log(12.213597446891887 * h / roughness)**2,
                 (1. - alpha) * (1. - beta)
             )
-    except ZeroDivisionError:  # when depth and roughness are pure python floats
+    except ZeroDivisionError:  # this err was raised when h and roughness are native python floats
         C3 = 1.
 
     return C1 * C2 * C3
